26f904596e00a0ef4b9f1e89ce436e7eb7888c5d
nicolehli <nicolehcli@gmail.com>
2017-12-23 17:38:39 -0800

    Create User model and configure passport

--- passportJS-auth-demo/app.js
+++ passportJS-auth-demo/app.js
@@ -1,18 +1,43 @@
-var express     = require("express"),
-    mongoose    = require("mongoose"),
-    app         = express();
+var express         = require('express'),
+    mongoose        = require('mongoose'),
+    app             = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/authDemoDB", {useMongoClient: true})
+var passport        = require('passport'),
+    bodyParser      = require('body-parser'),
+    LocalStrategy   = require('passport-local'),  // returns a constructor function, so we capitalize this variable
+    passportLocalMongoose   = require('passport-local-mongoose');
+
+var User            = require('./models/user');
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/authDemoDB', { useMongoClient: true })
+
 app.set('view engine', 'ejs')
-app.get("/", function(req, res){
-    res.render("home")
+app.use(require('express-session')({
+  secret: "Pressure makes diamonds",  // for encoding and decoding
+  resave: false,
+  saveUninitialized:  false
+}))
+
+// PASSPORT CONFIG
+app.use(passport.initialize())
+app.use(passport.session())
+// from passport-local-mongoose
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// HOME ROUTE
+app.get('/', function (req, res) {
+  res.render('home')
 })
 
-app.get("/secret", function(req, res){
-    res.render("secret")
+// SECRET ROUTE
+app.get('/secret', function (req, res) {
+  res.render('secret')
 })
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The server is running.")
+// LISTEN ON PORT
+var port = process.env.PORT | 3000
+app.listen(port, process.env.IP, function () {
+  console.log('The server is running.')
 })
